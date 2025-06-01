e81a0ef02d5a969ff6ad587cb05d160d65a9594a
ivanrj7j <ivanrj7j@gmail.com>
2025-06-01 13:05:25 +0530

    added new test

--- test.py
+++ test.py
@@ -2,6 +2,7 @@ import unittest
 from tests.modelTest import TestModels
 from tests.extractor import ExtractorTest
 from tests.crawler import TestCrawler, TestLinkedinCrawler
+from tests.contentCreator import TestContentCreator
 
 if __name__ == '__main__':
     unittest.main()
