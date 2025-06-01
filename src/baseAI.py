cb5e00f4c15d277a07bbb1fcd4c4e58c0955f591
ivanrj7j <ivanrj7j@gmail.com>
2025-06-01 13:08:06 +0530

    added option for structured output

--- src/baseAI.py
+++ src/baseAI.py
@@ -5,7 +5,7 @@ from typing import Any
 
 class BaseAI:
     def __init__(self, geminiKEY: str, prompt: str, schema: dict[str, Any] | None,
-                 requiredFields: list[str] | None, model: str = "gemini-2.0-flash-lite") -> None:
+                 requiredFields: list[str] | None, model: str = "gemini-2.0-flash-lite", structured:bool=True) -> None:
 
         responseSchema = genai.types.Schema(
             type=genai.types.Type.OBJECT,
@@ -16,7 +16,7 @@ class BaseAI:
         self.client = genai.Client(api_key=geminiKEY)
         self.model = model
         self.config = types.GenerateContentConfig(
-            response_mime_type="application/json",
+            response_mime_type="application/json" if structured else 'text/plain',
             response_schema=responseSchema,
             system_instruction=[
                 types.Part.from_text(text=prompt),
