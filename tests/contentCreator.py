13bbffbecc304afaa38e8640ac2adc1d6847a915
ivanrj7j <ivanrj7j@gmail.com>
2025-06-01 13:29:31 +0530

    content creator complete

--- tests/contentCreator.py
+++ tests/contentCreator.py
@@ -1,6 +1,7 @@
 from src.contentCreator import ContentCreator
 import unittest
 from dotenv import load_dotenv
+import json
 import os
 
 load_dotenv()
@@ -10,3 +11,23 @@ class TestContentCreator(unittest.TestCase):
         with open("prompts/contentCreator.md", encoding='utf-8') as f:
             prompt = f.read()
         creator = ContentCreator(os.environ["GEMINI_KEY"], prompt)
+
+    def test_response(self):
+        with open("prompts/contentCreator.md", encoding='utf-8') as f:
+            prompt = f.read()
+
+        with open("tests/posting.json") as f:
+            posting = json.load(f)
+
+        with open("tests/userData.json") as f:
+            candidateInfo = json.load(f)
+
+        inp = {
+            'candidateInfo':candidateInfo,
+            'posting': posting,
+            'customInstructions': ''
+        }
+
+        creator = ContentCreator(os.environ["GEMINI_KEY"], prompt)
+        creator.output(json.dumps(inp)) # testing input as json string
+        output = creator.output(inp)        
